428f61b90323ecbcb9496f80a1c4ad7ddc123710
Ram Babu Jaiswal <555ramjaiswal@gmail.com>
2023-01-26 16:36:22 +0530

    first program

---
+++ bitwiseoperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 6;
+    cout<<"a&b is : "<<(a&b)<<endl;
+    cout<<"a|b is : "<<(a|b)<<endl;
+    cout<<"~a is : "<<(~a)<<endl;
+    cout<<"a^b is : "<<(a^b)<<endl;
+    int i =7;
+    cout<<(i++)<<endl;
+    cout<<(++i)<<endl;
+    cout<<(i--)<<endl;
+    cout<<(--i)<<endl;
+    
+    
+}

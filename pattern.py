5fea80fe07acbcbdf944d8188679ffa836fb0a2f
Ramjaiswa5 <92715985+Ramjaiswa5@users.noreply.github.com>
2023-01-27 17:21:59 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,66 @@
+print("****************************************")
+print("****************************************")
+print("**                                    **")
+print("**         WELCOME TO PYTHON          **")
+print("**    DESIGN BY RAM BABU JAISWAL      **")
+print("**                                    **")
+print("****************************************")
+print("****************************************")
+
+print("\n")
+print("\n")
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end="")
+    print("\n")    
+
+print("\n")
+print("\n")
+
+for i in range(5,0,-1):
+    for j in range(1,i+1,):
+        print("*",end="")
+    print("\n")
+
+print("\n")
+print("\n")
+
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(" ",end="")
+    for k in range(1,i+1):
+        print("*",end="")
+    print("\n")
+
+print("\n")
+print("\n")
+
+for i in range(5,0,-1):
+    for j in range(1,6-i):
+        print(" ",end="")
+    for k in range(1,i+1):
+        print("*",end="")
+    print("\n")
+
+print("\n")
+print("\n")
+
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(" ",end="")
+    for k in range(0,2*i-1):
+        print("*",end="")
+    print("\n")
+
+print("\n")
+print("\n")
+
+for i in range(5,0,-1):
+    for j in range(1,6-i):
+        print(" ",end="")
+    for k in range(0,2*i-1):
+        print("*",end="")
+    print("\n")    
+    
+        

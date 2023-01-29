398148df10d8487edf7f2f9ff85b7df51a675d0c
Ram Babu Jaiswal <555ramjaiswal@gmail.com>
2023-01-29 23:25:54 +0530

    even or odd

---
+++ even_or_odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int z;
+    cout<<"enter the number to be checked "<<endl;
+    cin>>z;
+
+    if (z%2==0)
+    {
+        cout<<z<<" is even number"<<endl;
+    }
+    else
+    {
+        cout<<z<<" is odd number"<<endl;
+    }
+    
+}

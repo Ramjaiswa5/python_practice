a1476f3970940726ef6ee038d219fa11821d829f
Ram Babu Jaiswal <555ramjaiswal@gmail.com>
2023-01-31 18:54:29 +0530

    square root

---
+++ sqrt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    
+    int num;
+    cout<<"enter the number "<<endl;
+    cin>>num;
+
+    cout<<"the square root of "<<num<<" is "<<sqrt(num);
+}

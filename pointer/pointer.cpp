9887681ddabfda01602f0a52dd11462fde27f9b1
Ram Babu Jaiswal <555ramjaiswal@gmail.com>
2023-02-12 19:48:29 +0530

    pointer

---
+++ pointer/pointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num;
+    char ch = 'R';
+    cout<<"enter the number ";
+    cin>>num;
+    int *p = &num;
+    cout<<"you have enter "<<*p<<endl;
+    char *c = &ch;
+    cout<<"your character is "<<*c<<endl;
+
+}

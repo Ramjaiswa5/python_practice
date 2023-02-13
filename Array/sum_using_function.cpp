e6566006602c3f8204035fad14c392d2ddb79d34
Ram Babu Jaiswal <555ramjaiswal@gmail.com>
2023-02-13 22:45:29 +0530

    c++

--- Array/sum_using_function.cpp
+++ Array/sum_using_function.cpp
@@ -5,7 +5,10 @@ int sum(int n1, int n2){
 }
 int main(){
     int n1,n2;
+    cout<<"enter first value "<<endl;
     cin>>n1;
+    cout<<"enter second value "<<endl;
     cin>>n2;
-    cout<<sum(n1,n2);
+
+    cout<<"the sum of "<<n1<<" and "<<n2<<" is "<<sum(n1,n2);
 }

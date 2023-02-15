dd72b6b69298eb6e5cf5b28ee39656ded6cbdffc
Ram Babu Jaiswal <555ramjaiswal@gmail.com>
2023-02-15 22:41:28 +0530

    c++

--- pointer/pointer_in_array.cpp
+++ pointer/pointer_in_array.cpp
@@ -9,9 +9,10 @@ int main() {
     cout<<"address of first memory block is "<<&array<<endl;
 
     cout<<"value of first memory block is "<<*array<<endl;
-    cout<<"value of second memory block is "<<*(array+1)<<endl;
+    cout<<"value of second memory block is "<<*(array+1)<<endl; // +1 means jump one block or index
     cout<<"value of third memory block is "<<2[array]<<endl;
     cout<<"value of fourth memory block is "<<*(3+array)<<endl;
+    cout<<"value of fifth memory block is "<<array[4]<<endl;
 
     cout<<"address of tenth memory block is "<<&array[9]<<endl;
 

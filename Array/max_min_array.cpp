4e0105fc8db5bc8aae13e6d21ccd97f81bd14c87
Ram Babu Jaiswal <555ramjaiswal@gmail.com>
2023-02-17 23:55:32 +0530

    c++

--- Array/max_min_array.cpp
+++ Array/max_min_array.cpp
@@ -34,9 +34,9 @@ int getmax(int arr[],int n){
 int main() {
     int num[100];
     int size;
-    cout<<"enter the size of an array ";
+    cout<<"enter the size of an array "<<endl;
     cin>>size;
-
+    cout<<"enter all the element of an array "<<endl;
     for(int i = 0; i<size;i++){
         cin>>num[i];
     }
